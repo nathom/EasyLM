68a5e4a6bb5985426e4bd030101512999ca00eb1
Nathaniel Thomas <nathanthomas707@gmail.com>
2025-01-27 23:24:21 +0000

    revert pad

--- EasyLM/jax_utils.py
+++ EasyLM/jax_utils.py
@@ -33,12 +33,16 @@ def pad_tensor(tensor, target_divisor=4, axis=0, pad_value=0):
     Returns:
         The padded tensor.
     """
-    pad_shape = [(0,0)] * len(tensor.shape)
-    pad_shape[axis] = (0, target_divisor*cdiv(tensor.shape[axis], target_divisor))
-    return jnp.pad(tensor, pad_shape, mode='constant', constant_values=pad_value)
+    # print('tensor:', tensor)
+    size = tensor.shape[axis]
+    remainder = size % target_divisor
+    if remainder == 0:
+        return tensor
+    padding = target_divisor - remainder
+    pad_width = [(0, 0)] * len(tensor.shape)
+    pad_width[axis] = (0, padding)
+    return jnp.pad(tensor, pad_width, mode='constant', constant_values=pad_value)
 
-def cdiv(x, y):
-    return -(x//(-y))
 class JaxRNG(object):
     """ A convenient stateful Jax RNG wrapper. Can be used to wrap RNG inside
         pure function.
